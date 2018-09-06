0e2343a660bef3ec0cc55bb3070e4a7b1e0d8434
Mathias Roggeman <mathias.roggeman@hotmail.be>
2018-09-06 16:14:06 +0200

    redis import

--- movies/views.py
+++ movies/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import redis, random
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # Create your views here.
+def get(request):
+
